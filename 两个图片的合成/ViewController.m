eefc524bcd788f3f3a01a198cf6dd8bddaa1d96a
corepass <m15910883707@163.com>
2016-01-11 13:41:14 +0800

    图片合成

--- 两个图片的合成/ViewController.m
+++ 两个图片的合成/ViewController.m
@@ -16,9 +16,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
     // Do any additional setup after loading the view, typically from a nib.
+    UIImage * image = [UIImage imageNamed:@"one"];
+    UIImage * imageOne = [UIImage imageNamed:@"two"];
+    UIImageView * imageView = [[UIImageView alloc]init];
+    imageView.frame = self.view.frame;
+    imageView.backgroundColor = [UIColor greenColor];
+    imageView.image = [self addImage:image toImage:imageOne];
+    [self.view addSubview:imageView];
 }
-
+- (UIImage *)addImage:(UIImage *)image1 toImage:(UIImage *)image2 {
+    UIGraphicsBeginImageContext(image1.size);
+    
+    // Draw image1
+    [image1 drawInRect:CGRectMake(0, 0, image1.size.width, image1.size.height)];
+    
+    // Draw image2
+    [image2 drawInRect:CGRectMake(50, 50, image2.size.width, image2.size.height)];
+    
+    UIImage *resultingImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return resultingImage;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
